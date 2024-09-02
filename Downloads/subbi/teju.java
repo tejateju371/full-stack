f2d7e16655b90f9555a3861bfc8e598cb67da98b
teju <tejuteja371@gmail.com>
2024-09-02 11:24:42 +0530

    hello

---
+++ Downloads/subbi/teju.java
@@ -0,0 +1,11 @@
+public class teju {
+    
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        int c=30;
+
+     if (a>b&&a>c)?{System.out.println(a);}:if(b>c)? :b :c;
+
+    }
+}
